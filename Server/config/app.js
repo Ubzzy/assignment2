deabb62926f6b46ddf29ff0047df7d01e744ab45
Ubaid Delawala <66171227+Ubzzy@users.noreply.github.com>
2021-10-13 10:13:36 -0400

    Update app.js

--- Server/config/app.js
+++ Server/config/app.js
@@ -2,7 +2,7 @@ let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+let morgan = require('morgan');
 
 //Database setup
 let mongoose = require('mongoose');
