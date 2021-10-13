a692fa00487d8f21df8290fcf20be0efc00ba33e
Ubaid Delawala <delawalaubaid@gmail.com>
2021-10-13 07:00:58 -0400

    edit

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,6 +2,9 @@ const password = process.env.atlas_password;
 
 module.exports =
 {
-    "URI" : "mongodb+srv://Ubaid:" + password + "@store.mlyz6.mongodb.net/book_store"
+    "URI" : "mongodb+srv://Ubaid:Uubzzy5243@store.mlyz6.mongodb.net/book_store"
     //"URI" : "mongodb://localhost/book_store"
 }
+
+console.log(password)
+console.log(process.env.atlas_password)
