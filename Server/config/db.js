8929ccae3bf430dae342b3c6edc4f408ab269b8b
Ubaid Delawala <delawalaubaid@gmail.com>
2021-10-13 07:40:18 -0400

    fixing heroku

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,9 +2,6 @@ const password = process.env.atlas_password;
 
 module.exports =
 {
-    "URI" : "mongodb+srv://Ubaid:Uubzzy5243@store.mlyz6.mongodb.net/book_store"
+    "URI" : "mongodb+srv://Ubaid:Mongo2003@store.mlyz6.mongodb.net/book_store"
     //"URI" : "mongodb://localhost/book_store"
 }
-
-console.log(password)
-console.log(process.env.atlas_password)
