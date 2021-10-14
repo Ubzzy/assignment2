31c75bc85501d2aaed7ef71271a014b3d6ec85ac
Ubaid Delawala <delawalaubaid@gmail.com>
2021-10-13 21:47:54 -0400

    fixing heroku

--- Server/config/app.js
+++ Server/config/app.js
@@ -3,7 +3,6 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-require('Dotenv').config();
 
 //Database setup
 let mongoose = require('mongoose');
--- Server/config/db.js
+++ Server/config/db.js
@@ -1,5 +1,7 @@
+let password = process.env.password;
+
 module.exports =
 {
-    //"URI" : "mongodb+srv://Ubaid:Mongo2003@store.mlyz6.mongodb.net/book_store"
-    "URI" : "mongodb://localhost/book_store"
+    "URI" : "mongodb+srv://Ubaid:" + "Mongo2003" + "@store.mlyz6.mongodb.net/book_store"
+    //"URI" : "mongodb://localhost/book_store"
 }
